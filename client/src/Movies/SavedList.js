2fb1278605264e2f4c9b4f9b62572adde47948d5
guillsav <guillsav@gmail.com>
2019-04-01 16:39:24 -0400

    Saved Movie list turned into Links done

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,18 +1,16 @@
 import React, {Component} from 'react';
-import {Link} from 'react-router-dom';
+import {Link, NavLink} from 'react-router-dom';
 import SavedMovies from './SavedMovies';
 
 export default class SavedList extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return (
       <div className="saved-list">
         <h3>Saved Movies:</h3>
         {this.props.list.map(movie => (
-          <SavedMovies key={movie.id} title={movie.title} />
+          <NavLink key={movie.id} to={`/movies/${movie.id}`}>
+            <SavedMovies key={movie.id} title={movie.title} />
+          </NavLink>
         ))}
         <Link to="/" className="home-button">
           Home
